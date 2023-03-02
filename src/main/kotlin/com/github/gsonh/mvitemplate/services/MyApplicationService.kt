76e2f954de4baa3c01461ebb1b3d32dea43c59f5
GitHub Action <action@github.com>
2023-03-02 02:14:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/gsonh/mvitemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/gsonh/mvitemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.gsonh.mvitemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gsonh.mvitemplate.MyBundle
 
 class MyApplicationService {
 
